be546c191ae4dfa12442dc79e1a5e27ca648853d
NelsonJoshua03 <joshwords2020@gmal.com>
2025-05-26 22:59:12 +0530

    Remove apt-get and update asyncpg

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Install system dependencies
-apt-get update
-apt-get install -y python3-dev python3-pip
 
 # Install Python dependencies
 python3 -m pip install --upgrade pip
